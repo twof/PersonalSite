81f777e4a6bef3eecf7c203fe7196594e2554fae
twof <fabiobean2@gmail.com>
2016-05-10 11:57:32 -0700

    Made the controls more responsive. Now only animates on ball movement. Movement isn't allowed after the game ends. Both keys can now be released and have movement continue on the other paddle.

--- script.js
+++ script.js
@@ -70,8 +70,6 @@ function domloaded(){
   var paddleB = new Paddle(100, 30, c.width - 30, c.height/2);
 
   var map = [];
-  //window.ballAnimation();
-  document.onkeydown = document.onkeyup = interpretKeyPress;
 
   function interpretKeyPress(e) {
     if (map[87]) {
@@ -91,13 +89,11 @@ function domloaded(){
         paddleA.setPos(paddleA.getPosX(), paddleA.getPosY()+2);
       }
     }
-    redraw();
   }
 
   
   var ballAnimation = function(angle, direction){
     var id = setInterval(movement, 20);
-    console.log('beginning of the ball animation function');
     function movement(){
       if (ball.getPosX()+ball.getWidth() >= c.width) {
         ctx.fillText("Left Wins!",250 ,250);
@@ -113,7 +109,6 @@ function domloaded(){
         if ((ball.getPosY()+ball.getHeight())/2 < (paddleB.getPosY()+paddleB.getHeight())/2) {
           angle *= -1;
         }
-        console.log('direction set left');
         direction = "Left";
       }else if (ball.getPosX() <= paddleA.getPosX()+paddleA.getWidth() && ball.getPosY() <= paddleA.getPosY()+paddleA.getHeight() && ball.getPosY()+ball.getHeight() >= paddleA.getPosY()) {
         angle = ((ball.getPosY()+ball.getHeight())/2)/((paddleA.getPosY()+paddleA.getHeight())/2);
@@ -121,7 +116,6 @@ function domloaded(){
         if ((ball.getPosY+ball.getHeight())/2 < (paddleA.getPosY()+paddleA.getHeight())/2) {
           angle *= -1;
         }
-        console.log('direction set right');
         direction = "Right";
       }else if (ball.getPosY()+ball.getHeight() >= c.height) {
         angle-=0.9;
@@ -137,13 +131,12 @@ function domloaded(){
 
       ball.setPos(ball.getPosX(), ball.getPosY()+angle);
       redraw();
+      interpretKeyPress();
     }
-    console.log('end of the ball animation function');
   };
 
 
   var redraw = function(){
-    console.log('redraw');
     //clear canvas
     ctx.clearRect(0, 0, c.width, c.height);
     //draw ball
@@ -158,8 +151,11 @@ function domloaded(){
 
   document.body.addEventListener("keydown", function (e) {
     map[e.keyCode] = true;
+    console.log(e.keyCode + ' keydown');
   });
+
   document.body.addEventListener("keyup", function (e) {
     map[e.keyCode] = false;
+    console.log(e.keyCode + ' keyup');
   });
 }
